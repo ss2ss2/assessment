9c071cf82f7e70256f34d2178d4680b8a369d1be
ss2ss2 <usa.saru.2525@gmail.com>
2026-03-18 15:34:43 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -15,14 +15,30 @@ assessmentButton.addEventListener(
 
     // 診断結果表示エリアの作成
     resultDivision.innerText = '';
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivision.appendChild(header);
+    // headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    // bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    // headerDivision と bodyDivision を resultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
+
+    // ツイートエリアの作成
+
 
     // ツイートエリアの作成
     tweetDivision.innerText = '';
@@ -46,29 +62,29 @@ assessmentButton.addEventListener(
 userNameInput.addEventListener(
   'keydown',
   (event) => {
-    if(event.code === 'Enter') {
+    if (event.code === 'Enter') {
       assessmentButton.dispatchEvent(new Event('click'))
     }
   }
 )
 
 const answers = [
-'###userName###のいいところは声です。###userName###の特徴的な声は皆を惹きつけ、心に残ります。',
-'###userName###のいいところはまなざしです。###userName###に見つめられた人は、気になって仕方がないでしょう。',
-'###userName###のいいところは情熱です。###userName###の情熱に周りの人は感化されます。',
-'###userName###のいいところは厳しさです。###userName###の厳しさがものごとをいつも成功に導きます。',
-'###userName###のいいところは知識です。博識な###userName###を多くの人が頼りにしています。',
-'###userName###のいいところはユニークさです。###userName###だけのその特徴が皆を楽しくさせます。',
-'###userName###のいいところは用心深さです。###userName###の洞察に、多くの人が助けられます。',
-'###userName###のいいところは見た目です。内側から溢れ出る###userName###の良さに皆が気を惹かれます。',
-'###userName###のいいところは決断力です。###userName###がする決断にいつも助けられる人がいます。',
-'###userName###のいいところは思いやりです。###userName###に気をかけてもらった多くの人が感謝しています。',
-'###userName###のいいところは感受性です。###userName###が感じたことに皆が共感し、わかりあうことができます。',
-'###userName###のいいところは節度です。強引すぎない###userName###の考えに皆が感謝しています。',
-'###userName###のいいところは好奇心です。新しいことに向かっていく###userName###の心構えが多くの人に魅力的に映ります。',
-'###userName###のいいところは気配りです。###userName###の配慮が多くの人を救っています。',
-'###userName###のいいところはそのすべてです。ありのままの###userName###自身がいいところなのです。',
-'###userName###のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されています。'
+  '###userName###のいいところは声です。###userName###の特徴的な声は皆を惹きつけ、心に残ります。',
+  '###userName###のいいところはまなざしです。###userName###に見つめられた人は、気になって仕方がないでしょう。',
+  '###userName###のいいところは情熱です。###userName###の情熱に周りの人は感化されます。',
+  '###userName###のいいところは厳しさです。###userName###の厳しさがものごとをいつも成功に導きます。',
+  '###userName###のいいところは知識です。博識な###userName###を多くの人が頼りにしています。',
+  '###userName###のいいところはユニークさです。###userName###だけのその特徴が皆を楽しくさせます。',
+  '###userName###のいいところは用心深さです。###userName###の洞察に、多くの人が助けられます。',
+  '###userName###のいいところは見た目です。内側から溢れ出る###userName###の良さに皆が気を惹かれます。',
+  '###userName###のいいところは決断力です。###userName###がする決断にいつも助けられる人がいます。',
+  '###userName###のいいところは思いやりです。###userName###に気をかけてもらった多くの人が感謝しています。',
+  '###userName###のいいところは感受性です。###userName###が感じたことに皆が共感し、わかりあうことができます。',
+  '###userName###のいいところは節度です。強引すぎない###userName###の考えに皆が感謝しています。',
+  '###userName###のいいところは好奇心です。新しいことに向かっていく###userName###の心構えが多くの人に魅力的に映ります。',
+  '###userName###のいいところは気配りです。###userName###の配慮が多くの人を救っています。',
+  '###userName###のいいところはそのすべてです。ありのままの###userName###自身がいいところなのです。',
+  '###userName###のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されています。'
 ];
 
 /**
@@ -86,7 +102,7 @@ function assessment(userName) {
   // 文字のコード番号の合計を回答の数で割って添え字の数値を求める
   const index = sumOfCharCode % answers.length;
   let result = answers[index];
-  
+
   result = result.replaceAll('###userName###', userName);
   return result;
 }
@@ -99,7 +115,7 @@ function test() {
   console.log('太郎');
   console.assert(
     assessment('太郎') ===
-      '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+    '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
     '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
   );
 
@@ -107,7 +123,7 @@ function test() {
   console.log('次郎');
   console.assert(
     assessment('次郎') ===
-      '次郎のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる次郎が皆から評価されています。',
+    '次郎のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる次郎が皆から評価されています。',
     '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
   );
 
@@ -115,10 +131,10 @@ function test() {
   console.log('花子');
   console.assert(
     assessment('花子') ===
-      '花子のいいところはまなざしです。花子に見つめられた人は、気になって仕方がないでしょう。',
+    '花子のいいところはまなざしです。花子に見つめられた人は、気になって仕方がないでしょう。',
     '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
   );
-  
+
   console.log('診断結果の文章のテスト終了');
 }
 
